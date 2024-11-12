5805010594ac544b248f7393648f4a7af3ca8c27
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 10:08:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
         width: 800,                // Chiều rộng của Flipbook
         height: 600,               // Chiều cao của Flipbook
         autoCenter: true,          // Căn giữa flipbook
-        display: 'single',         // Hiển thị một trang tại một thời điểm
+        display: 'double',         // Hiển thị một trang tại một thời điểm
         mobile: true,              // Kích hoạt chế độ cho điện thoại di động
-        duration: 1000,            // Thời gian cho mỗi lần lật trang
+        duration: 800,            // Thời gian cho mỗi lần lật trang
         gradients: true,           // Hiệu ứng gradient cho lật trang
-        elevation: 50              // Độ sâu của lật trang
+        elevation: 20              // Độ sâu của lật trang
     });
 });
 
