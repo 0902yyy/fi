30dd5360c474c4cb8d96c8d21e7c944dc19c8b0e
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 11:21:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,47 @@
-$(document).ready(function() {
-flipbook.turn({
-
-        // Magazine width
-
-        width: 100%,
-
-        // Magazine height
-
-        height: 500,
-
-        // Duration in millisecond
-
-        duration: 1000,
-
-        // Enables gradients
-
-        gradients: true,
-
-        // Auto center this flipbook
-
-        autoCenter: true,
-
-        // Elevation from the edge of the flipbook when turning a page
-
-        elevation: 50,
-
-        // The number of pages
-
-        pages: 12,
-
-
-        // Events
-
-        when: {
-            turning: function(event, page, view) {
-
-                var book = $(this),
-                currentPage = book.turn('page'),
-                pages = book.turn('pages');
-
-                // Update the current URI
-
-                Hash.go('page/' + page).update();
-
-                // Show and hide navigation buttons
-
-                disableControls(page);
-
-            },
-
-            turned: function(event, page, view) {
-
-                disableControls(page);
-
-                $(this).turn('center');
-
-                $('#slider').slider('value', getViewNumber($(this), page));
-
-                if (page==1) {
-                    $(this).turn('peel', 'br');
-                }
-
-            },
-
-            missing: function (event, pages) {
-
-                // Add pages that aren't in the magazine
-
-                for (var i = 0; i < pages.length; i++)
-                    addPage(pages[i], $(this));
-
-            }
-        }
-
-});
-    //change from single to double page
-    $(window).width(function(){
-  var win = $(this); //this = window
-  if (win.width() >= 820) { flipbook.turn('display','double');}
-  else {
-    flipbook.turn('display','single');
-  }
-});
-$(window).resize(function(){
-var win = $(this); //this = window
-if (win.width() >= 820) { flipbook.turn('display','double');}
-else {
-  flipbook.turn('display','single');
+html, body {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f0f0f0;
 }
-});
+
+#flipbook {
+    width: 90%;
+    height: 80%;
+    max-width: 1000px;
+    max-height: 600px;
+    background-color: #fff;
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
+    position: relative;
+    overflow: hidden;
+    margin: 0 auto;
+}
+
+.page {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    position: relative;
+    overflow: hidden;
+}
+
+.page img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+/* Responsive adjustment */
+@media screen and (max-width: 600px) {
+    #flipbook {
+        width: 100%;
+        height: 100%;
+        max-width: none;
+        max-height: none;
+    }
+}
