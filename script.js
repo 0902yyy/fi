2ef5410d4140213073c183cb38d8e3cfbfc7ef9d
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 11:48:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
 $(document).ready(function() {
-  // Khởi tạo Flipbook
-  $('#flipbook').turn({
-    width: 800,  // Đặt chiều rộng cho flipbook
-    height: 600, // Đặt chiều cao cho flipbook
-    autoCenter: true, // Tự động căn giữa flipbook
-    pages: 22, // Số lượng trang
-    display: 'double', // Hiển thị chế độ 2 trang
-    duration: 1000,  // Thời gian chuyển trang
-    gradients: true, // Hiệu ứng gradient
-    elevation: 50, // Độ nổi khi lật trang
-    when: {
-      turning: function(event, page, view) {
-        var book = $(this),
-            currentPage = book.turn('page'),
-            pages = book.turn('pages');
-      },
+  $("#flipbook").turn({
+    width: "100%",
+    height: "100%",
+    autoCenter: true,
+    responsive: true
+  });
 
-      turned: function(event, page, view) {
-        $(this).turn('center');
-      },
-
-      missing: function(event, pages) {
-        // Thêm trang khi còn thiếu
-        for (var i = 0; i < pages.length; i++) {
-          addPage(pages[i], $(this));
-        }
-      }
-    }
+  $(window).resize(function() {
+    $("#flipbook").turn("size", $("#flipbook").width(), $("#flipbook").height());
   });
 });
