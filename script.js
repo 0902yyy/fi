33aeb9399dca1fa146e314147b58d3e62123cb46
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 10:45:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+$(document).ready(function() {
+    // Initialize the flipbook with Turn.js
+    $("#flipbook").turn({
+        autoCenter: true
+    });
+
+    // Handle window resize
+    var flipbookEL = document.getElementById('flipbook');
+    
+    window.addEventListener('resize', function () {
+        flipbookEL.style.width = '';
+        flipbookEL.style.height = '';
+        $(flipbookEL).turn('size', flipbookEL.clientWidth, flipbookEL.clientHeight);
+    });
+});
 
