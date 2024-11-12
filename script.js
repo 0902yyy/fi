2cea3aa830bd1ad9b3a26c830e722eee430378a0
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 11:17:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,90 @@
-$(document).ready(function() {
-  // Kiểm tra Turn.js đã được tải chưa
-  if ($.isFunction($.fn.turn)) {
-    $('#flipbook').turn({
-      width: 1000,           // Đặt chiều rộng flipbook
-      height: 500,           // Đặt chiều cao flipbook
-      autoCenter: true,      // Tự động căn giữa flipbook
-      display: responsive,     // Hiển thị 2 trang cùng lúc
-      acceleration: true,    // Tăng tốc để mượt mà hơn
-      gradients: true,       // Hiệu ứng gradient khi lật
-      duration: 800          // Thời gian lật trang
-    });
-  } else {
-    console.error("Turn.js chưa được tải đúng.");
+flipbook.turn({
+
+        // Magazine width
+
+        width: 100%,
+
+        // Magazine height
+
+        height: 500,
+
+        // Duration in millisecond
+
+        duration: 1000,
+
+        // Enables gradients
+
+        gradients: true,
+
+        // Auto center this flipbook
+
+        autoCenter: true,
+
+        // Elevation from the edge of the flipbook when turning a page
+
+        elevation: 50,
+
+        // The number of pages
+
+        pages: 12,
+
+
+        // Events
+
+        when: {
+            turning: function(event, page, view) {
+
+                var book = $(this),
+                currentPage = book.turn('page'),
+                pages = book.turn('pages');
+
+                // Update the current URI
+
+                Hash.go('page/' + page).update();
+
+                // Show and hide navigation buttons
+
+                disableControls(page);
+
+            },
+
+            turned: function(event, page, view) {
+
+                disableControls(page);
+
+                $(this).turn('center');
+
+                $('#slider').slider('value', getViewNumber($(this), page));
+
+                if (page==1) {
+                    $(this).turn('peel', 'br');
+                }
+
+            },
+
+            missing: function (event, pages) {
+
+                // Add pages that aren't in the magazine
+
+                for (var i = 0; i < pages.length; i++)
+                    addPage(pages[i], $(this));
+
+            }
+        }
+
+});
+    //change from single to double page
+    $(window).width(function(){
+  var win = $(this); //this = window
+  if (win.width() >= 820) { flipbook.turn('display','double');}
+  else {
+    flipbook.turn('display','single');
   }
+});
+$(window).resize(function(){
+var win = $(this); //this = window
+if (win.width() >= 820) { flipbook.turn('display','double');}
+else {
+  flipbook.turn('display','single');
+}
 });
