16de8d115079229841413bb4abc4e0548635c2e9
0902yyy <thaiminhthu0902@gmail.com>
2024-11-12 10:00:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         width: 800,                // Chiều rộng của Flipbook
         height: 600,               // Chiều cao của Flipbook
         autoCenter: true,          // Căn giữa flipbook
-        display: 'double',         // Hiển thị một trang tại một thời điểm
+        display: 'single',         // Hiển thị một trang tại một thời điểm
         mobile: true,              // Kích hoạt chế độ cho điện thoại di động
         duration: 1000,            // Thời gian cho mỗi lần lật trang
         gradients: true,           // Hiệu ứng gradient cho lật trang
