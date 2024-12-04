6417f95eaad29ed296ef4024e7bd2538504d50ca
0902yyy <thaiminhthu0902@gmail.com>
2024-12-04 09:46:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
   // Kiểm tra Turn.js đã được tải chưa
-  if ($.isFunction($.fn.turn)) {
-    // Kiểm tra thiết bị là mobile hay desktop
-     const screenWidth = $(window).width();
-    const screenHeight = $(window).height();
-
-    const flipbookHeight = isMobile ? screenHeight * 0.8 : 500;  // Mobile: chiều cao 80% màn hình
-    const flipbookWidth = isMobile ? screenWidth : flipbookHeight * 2; // Mobile: chiều rộng tự động căn chỉnh, Desktop: gấp đôi chiều cao
-
+ 
 
     // Khởi tạo Turn.js
     $('#flipbook').turn({
